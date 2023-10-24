9aabd3cb49264de10a07c545e2ba1784f58e9cf9
Alim <coder34521@gmail.com>
2023-10-24 09:30:21 +0530

    Changed to Atlas

--- server/db.js
+++ server/db.js
@@ -2,7 +2,9 @@
 const mongoose = require("mongoose")
 
 // The url found at the starting page of MongoDbCompass, replace the localhost with 0.0.0.0 and after the / write the name of the collection in which you want to store the data, in this case it is "Google_Keep"
-const monogoURI = "mongodb://0.0.0.0:27017/Google_Keep";
+// const monogoURI = "mongodb://0.0.0.0:27017/Google_Keep";
+const monogoURI = "mongodb+srv://coder34521:x9lEHQ2lNnpWb14z@cluster0.pq9euua.mongodb.net/Google_Keep";
+
 
 const connectToMongoDB = () => {
     // inbuilt mongoose function to connect to MongoDb
